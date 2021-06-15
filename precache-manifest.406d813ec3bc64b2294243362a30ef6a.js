926c418ee3020948e55bf050eecc9d1fe43808ae
HyunJun-Yoon <HyunJun-Yoon@users.noreply.github.com>
2021-06-15 18:21:32 +0000

    deploy: 8b42dd38cdb833f5dd69a2ee70936695352c13e7

---
+++ precache-manifest.406d813ec3bc64b2294243362a30ef6a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a0316ffa723308c50072c2533275b9a",
+    "url": "https://HyunJun-Yoon.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "729772c12aca63728c0c",
+    "url": "https://HyunJun-Yoon.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "40dd73d8b7adb93f2e1b",
+    "url": "https://HyunJun-Yoon.github.io/task-management-frontend/static/js/2.9d1dd7c7.chunk.js"
+  },
+  {
+    "revision": "729772c12aca63728c0c",
+    "url": "https://HyunJun-Yoon.github.io/task-management-frontend/static/js/main.e5594cdd.chunk.js"
+  },
+  {
+    "revision": "bddc197d0ab933109bee",
+    "url": "https://HyunJun-Yoon.github.io/task-management-frontend/static/js/runtime~main.3b782a68.js"
+  }
+]);
